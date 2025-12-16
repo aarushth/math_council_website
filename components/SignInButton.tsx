fbf4a0f85f564abf46949c509d4e6fa949dcc77e
Aarush Thadani <65953492+aarushth@users.noreply.github.com>
2025-12-16 20:26:27 +0000

    fixed db and sign in redirect

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -1,12 +1,25 @@
 'use client'
+
 import { signIn } from 'next-auth/react'
 import { FaGoogle } from 'react-icons/fa'
 import { Button } from '@heroui/button'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 function SignInButton() {
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+
+    const query = searchParams?.toString()
+    const callbackUrl = query ? `${pathname}?${query}` : pathname || undefined
+
     return (
         <Button
-            onPress={() => signIn('google', { prompt: 'select_account' })}
+            onPress={() =>
+                signIn('google', {
+                    callbackUrl,
+                    prompt: 'select_account',
+                })
+            }
             variant="ghost"
             color="primary"
         >
@@ -15,4 +28,5 @@ function SignInButton() {
         </Button>
     )
 }
+
 export default SignInButton
